f67be783ead85e748d766e51b3bdc1066f4cd538
LizaKoval <liza.koval.01@bk.ru>
2023-02-06 22:57:40 +0300

    2-st task
    file 1_1.py should be named as 2_1.py

--- 1_1.py
+++ 1_1.py
@@ -1,3 +1,4 @@
 msg = input("Введите, пожалуйста, текст с '-': ")
 print(f'{msg.replace(" ","-")}')  # 1st way (by replace method)
-print(f'{"-".join(list(msg.split()))}')  # 2nd way (splitting string into words by split() method then joining list of words in whole string)
+print(f'{"-".join(list(msg.split()))}')  # 2nd way (splitting string into words by split()
+                                         # method then joining list of words in whole string)
